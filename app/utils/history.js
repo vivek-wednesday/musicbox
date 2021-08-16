e9b02bfd65cfc5aa489c8a72661dbf19f755db9d
Vivek <vivekpy@outlook.com>
2021-08-16 13:28:49 +0530

    Fix naming

--- app/utils/history.js
+++ app/utils/history.js
@@ -15,10 +15,6 @@ if (process.env.NODE_ENV === 'uat') {
       }
     }
   });
-} else if (process.env.NODE_ENV === 'production') {
-    baseUrl = '/react-template'
-} else {
-    baseUrl = '/'
 }
 
 const history = createBrowserHistory({ basename: baseUrl });
